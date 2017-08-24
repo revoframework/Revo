9673cb763b9b2a8700923e90003ea10e099a0e24
Jan Drozen <jan.drozen@aspas.eu>
2017-08-24 15:21:35 +0200

    tenancy for classified and eventsourced entities (GTREVO-27)

---
+++ GTRevo.Infrastructure.Core/Tenancy/TenantClassifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTRevo.Infrastructure.Core.Domain.Basic;
+
+namespace GTRevo.Infrastructure.Core.Tenancy
+{
+    public abstract class TenantClassifier: BasicClassifier, ITenantOwned
+    {
+        protected TenantClassifier(Guid id, string code, ITenant tenant) : base(id, code)
+        {
+            TenantId = tenant?.Id;
+        }
+
+        protected TenantClassifier(): base()
+        {
+        }
+
+        public Guid? TenantId { get; }
+    }
+}
---
+++ GTRevo.Infrastructure.Core/Tenancy/TenantEventSourcedAggregateRoot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GTRevo.Infrastructure.Core.Domain.EventSourcing;
+
+namespace GTRevo.Infrastructure.Core.Tenancy
+{
+    public abstract class TenantEventSourcedAggregateRoot: EventSourcedAggregateRoot, ITenantOwned
+    {
+        protected TenantEventSourcedAggregateRoot(Guid id, ITenant tenant) : base(id)
+        {
+            TenantId = tenant?.Id;
+        }
+
+        public Guid? TenantId { get; }
+    }
+}
