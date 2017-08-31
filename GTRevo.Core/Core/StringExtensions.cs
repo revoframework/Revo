eab2b33e836d341480a4e36f348f544f2b823c8a
Jan Drozen <jan.drozen@aspas.eu>
2017-08-31 15:08:08 +0200

    simple extension method to omit trailing backslash

--- GTRevo.Core/Core/StringExtensions.cs
+++ GTRevo.Core/Core/StringExtensions.cs
@@ -23,5 +23,21 @@ namespace GTRevo.Core.Core
         {
             return haystack.LastIndexOf(needle, StringComparison.Ordinal);
         }
+
+        /// <summary>
+        /// It the last character of given string is a backslash, result is the string without this backslash. Otherwise result is the input string.
+        /// </summary>
+        /// <param name="path">String to be modified.</param>
+        /// <returns>String without last backslash.</returns>
+        public static string RemoveLastBackslash(this string path)
+        {
+            if (path == null)
+                return null;
+            if (path[path.Length - 1] == '\\')
+            {
+                return path.Substring(0, path.Length - 1);
+            }
+            return path;
+        }
     }
 }
