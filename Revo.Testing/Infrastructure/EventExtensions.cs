52a6a7c942474fc8dc4d8f97f868e9cd9f412dd1
Martin Zima <mail@zimamartin.cz>
2018-03-31 20:57:56 +0200

    (GTREVO-79) added test helper method EventExtensions.ToAggregateEventMessages

--- Revo.Testing/Infrastructure/EventExtensions.cs
+++ Revo.Testing/Infrastructure/EventExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Revo.Core.Events;
+using Revo.Domain.Events;
 
 namespace Revo.Testing.Infrastructure
 {
@@ -11,6 +14,22 @@ namespace Revo.Testing.Infrastructure
             Type eventType = @event.GetType();
             Type messageType = typeof(EventMessageDraft<>).MakeGenericType(eventType);
             return (IEventMessageDraft<TEvent>) messageType.GetConstructor(new[] {eventType}).Invoke(new[] {@event});
+        }
+
+        public static List<IEventMessageDraft<DomainAggregateEvent>> ToAggregateEventMessages(this IEnumerable<DomainAggregateEvent> events,
+            Guid? aggregateClassId = null, long firstSequenceNumber = 0)
+        {
+            return events.Select((x, i) =>
+            {
+                var message = x.ToMessageDraft();
+                message.SetMetadata(BasicEventMetadataNames.StreamSequenceNumber, (i + firstSequenceNumber + 1).ToString());
+
+                if (aggregateClassId != null)
+                {
+                    message.SetMetadata(BasicEventMetadataNames.AggregateClassId, aggregateClassId.ToString());
+                }
+                return message;
+            }).ToList();
         }
     }
 }
