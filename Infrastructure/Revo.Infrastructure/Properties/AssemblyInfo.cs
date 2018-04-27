1118042ca14bf1cb690b7c2e80a9fafabd1131bf
Martin Zima <mail@zimamartin.cz>
2018-04-27 14:27:12 +0200

    fixed project packages

--- Infrastructure/Revo.Infrastructure/Properties/AssemblyInfo.cs
+++ Infrastructure/Revo.Infrastructure/Properties/AssemblyInfo.cs
@@ -36,3 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("Revo.Infrastructure.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
