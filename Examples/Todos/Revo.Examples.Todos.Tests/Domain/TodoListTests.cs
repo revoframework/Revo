ef62280cd7cf4140ea63aec4aeb0c184d6dd30bf
Martin Zima <mail@zimamartin.cz>
2019-01-14 17:59:34 +0100

    added tests for Todos example

---
+++ Examples/Todos/Revo.Examples.Todos.Tests/Domain/TodoListTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Revo.Domain.Tenancy.Events;
+using Revo.Examples.Todos.Domain;
+using Revo.Examples.Todos.Domain.Events;
+using Revo.Testing.Infrastructure;
+using Xunit;
+
+namespace Revo.Examples.Todos.Tests.Domain
+{
+    public class TodoListTests
+    {
+        private TodoList sut;
+
+        public TodoListTests()
+        {
+            sut = new TodoList(Guid.Parse("E16EF2AB-20FF-438E-A1CE-48E6A8BDC7B2"), "My todo list");
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Constructor(bool loadEventsOnly)
+        {
+            sut.AssertConstructorEvents(x =>
+                {
+                    x.Id.Should().Be(Guid.Parse("E16EF2AB-20FF-438E-A1CE-48E6A8BDC7B2"));
+                    x.Name.Should().Be("My todo list");
+
+                }, loadEventsOnly,
+                new TodoListRenamedEvent("My todo list"));
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Rename(bool loadEventsOnly)
+        {
+            sut.AssertEvents(
+                x =>
+                {
+                    x.Rename("Groceries");
+                },
+                x =>
+                {
+                    x.Name.Should().Be("Groceries");
+                },
+                loadEventsOnly,
+                new TodoListRenamedEvent("Groceries"));
+        }
+    }
+}
