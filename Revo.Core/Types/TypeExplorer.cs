82cd906ddcc5bc1ea81165dfdea7187e4038da78
Martin Zima <mail@zimamartin.cz>
2020-08-06 14:37:14 +0200

    catching ReflectionTypeLoadException exceptions in TypeExplorer

--- Revo.Core/Types/TypeExplorer.cs
+++ Revo.Core/Types/TypeExplorer.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using NLog;
 
 namespace Revo.Core.Types
 {
     public class TypeExplorer : ITypeExplorer
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public virtual IEnumerable<Assembly> GetAllReferencedAssemblies()
         {
             return AppDomain.CurrentDomain.GetAssemblies();
@@ -15,7 +18,18 @@ namespace Revo.Core.Types
         public IEnumerable<Type> GetAllTypes()
         {
             var assemblies = GetAllReferencedAssemblies();
-            return assemblies.SelectMany(x => x.GetTypes());
+            return assemblies.SelectMany(x =>
+            {
+                try
+                {
+                    return x.GetTypes();
+                }
+                catch (ReflectionTypeLoadException e)
+                {
+                    Logger.Warn(e, $"Could not load types from assembly {x}, loader exceptions: {string.Join("\n", e.LoaderExceptions.Select(x => x.ToString()))}");
+                    return new Type[0];
+                }
+            });
         }
 
         public Type FindType(string typeName)
