2b9e15aa881f1eb2748d117064b14504e81d4156
Martin Zima <mail@zimamartin.cz>
2020-09-08 14:47:39 +0200

    added TenantReadModelBase (common base for tenant-owned non-entity read models)

---
+++ Revo.Domain/Tenancy/TenantReadModelBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Revo.Domain.ReadModel;
+
+namespace Revo.Domain.Tenancy
+{
+    public abstract class TenantReadModelBase : ReadModelBase, ITenantOwned
+    {
+        public Guid? TenantId { get; set; }
+    }
+}
