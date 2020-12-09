cc695513737250560c2fdf07377993ace36c9817
Martin Zima <mail@zimamartin.cz>
2020-12-10 00:11:07 +0100

    added IEFCoreReadRepository.FromSqlInterpolated<T> and FromSqlRaw<T> (which apply repository filters, unlike existing IEFCoreDatabaseAccess methods)

--- Providers/EFCore/Revo.EFCore/DataAccess/Entities/IEFCoreReadRepository.cs
+++ Providers/EFCore/Revo.EFCore/DataAccess/Entities/IEFCoreReadRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Query;
 using Revo.DataAccess.Entities;
 
 namespace Revo.EFCore.DataAccess.Entities
@@ -7,5 +8,10 @@ namespace Revo.EFCore.DataAccess.Entities
     public interface IEFCoreReadRepository : IReadRepository
     {
         IEFCoreDatabaseAccess DatabaseAccess { get; }
+
+        IQueryable<T> FromSqlInterpolated<T>([NotParameterized] FormattableString sql)
+            where T : class;
+        IQueryable<T> FromSqlRaw<T>([NotParameterized] string sql, params object[] parameters)
+            where T : class;
     }
 }
