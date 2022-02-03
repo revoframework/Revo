893824cd5620473cd6ccd9c32295d6a84b5f799b
Martin Zima <mail@zimamartin.cz>
2022-02-03 11:08:25 +0100

    fixed InMemoryCrudRepository returning non-saved entries & added tests

--- Tests/Revo.DataAccess.Tests/InMemory/InMemoryCrudRepositoryTests.cs
+++ Tests/Revo.DataAccess.Tests/InMemory/InMemoryCrudRepositoryTests.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using Revo.DataAccess.InMemory;
+using Revo.Domain.ReadModel;
+using System;
+using Xunit;
 
 namespace Revo.DataAccess.Tests.InMemory
 {
     public class InMemoryCrudRepositoryTests
     {
+        private InMemoryCrudRepository sut = new InMemoryCrudRepository();
+        
+        [Fact]
+        public void FindAll_ReturnsAfterSaving()
+        {
+            var entity1 = new Entity1() { Id = Guid.NewGuid() };
+            sut.Add(entity1);
+            sut.SaveChanges();
+            sut.FindAll<Entity1>().Should().HaveCount(1).And.Contain(entity1);
+        }
+
+        [Fact]
+        public void FindAll_ReturnsOnlySaved()
+        {
+            var entity1 = new Entity1() { Id = Guid.NewGuid() };
+            sut.Add(entity1);
+            sut.FindAll<Entity1>().Should().BeEmpty();
+        }
+        
+        [Fact]
+        public void FirstOrDefault_ReturnsMatching()
+        {
+            var entity1 = new Entity1() { Id = Guid.NewGuid() };
+            var entity2 = new Entity1() { Id = Guid.NewGuid() };
+            sut.AddRange(new[] { entity1, entity2 });
+            sut.SaveChanges();
+            sut.FirstOrDefault<Entity1>(x => x.Id == entity1.Id).Should().Be(entity1);
+        }
+
+        [Fact]
+        public void FirstOrDefault_NullIfNotFound()
+        {
+            sut.FirstOrDefault<Entity1>(x => x.Id == Guid.NewGuid()).Should().BeNull();
+        }
+
+        [Fact]
+        public void FirstOrDefault_ReturnsOnlySaved()
+        {
+            var entity1 = new Entity1() { Id = Guid.NewGuid() };
+            sut.Add(entity1);
+            sut.FirstOrDefault<Entity1>(x => x.Id == entity1.Id).Should().BeNull();
+        }
+
+        class Entity1 : EntityReadModel {}
     }
 }
