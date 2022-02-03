e2d192360727abd733819f1b3a142d1f8272aca5
Martin Zima <mail@zimamartin.cz>
2022-02-03 11:09:13 +0100

    fixed BufferedNotificationStore creating the same buffer multiple times

--- Extensions/Tests/Revo.Extensions.Notifications.Tests/Channels/Bufferring/BufferedNotificationStoreTests.cs
+++ Extensions/Tests/Revo.Extensions.Notifications.Tests/Channels/Bufferring/BufferedNotificationStoreTests.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using MoreLinq;
 using NSubstitute;
 using Revo.DataAccess.InMemory;
 using Revo.Extensions.Notifications.Channels.Buffering;
 using Revo.Extensions.Notifications.Model;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Revo.Extensions.Notifications.Tests.Channels.Bufferring
@@ -32,33 +34,50 @@ namespace Revo.Extensions.Notifications.Tests.Channels.Bufferring
             await sut.Add(new SerializedNotification() { NotificationClassName = "Notification1", NotificationJson = "{}" },
                 buffer.Name, now, "governor1", "pipeline1");
 
-            Assert.Equal(1, inMemoryCrudRepository.FindAllWithAdded<BufferedNotification>().Count());
-            Assert.Single(inMemoryCrudRepository.FindAllWithAdded<BufferedNotification>(),
-                x => x.NotificationClassName == "Notification1"
-                     && x.NotificationJson == "{}"
-                     && x.Buffer == buffer
-                     && x.TimeQueued == now
-                     && x.Id != Guid.Empty);
-            Assert.Equal(1, inMemoryCrudRepository.FindAll<NotificationBuffer>().Count());
+            inMemoryCrudRepository.FindAllWithAdded<BufferedNotification>()
+                .Should().HaveCount(1)
+                .And.ContainSingle(x => x.NotificationClassName == "Notification1"
+                                        && x.NotificationJson == "{}"
+                                        && x.Buffer == buffer
+                                        && x.TimeQueued == now
+                                        && x.Id != Guid.Empty);
         }
 
         [Fact]
         public async Task SendNotificationAsync_CreatesNewBuffer()
         {
-            Guid bufferGovernorId = Guid.NewGuid();
-            Guid notificationPipelineId = Guid.NewGuid();
-
             await sut.Add(new SerializedNotification() { NotificationClassName = "Notification1", NotificationJson = "{}" },
                 "buffer1", DateTime.Now, "governor1", "pipeline1");
 
-            Assert.Equal(1, inMemoryCrudRepository.FindAllWithAdded<NotificationBuffer>().Count());
-            Assert.Single(inMemoryCrudRepository.FindAllWithAdded<NotificationBuffer>(),
-                x => x.Name == "buffer1"
-                && x.GovernorName == "governor1"
-                && x.PipelineName == "pipeline1");
-            Assert.Equal(1, inMemoryCrudRepository.FindAllWithAdded<BufferedNotification>().Count());
-            Assert.Single(inMemoryCrudRepository.FindAllWithAdded<BufferedNotification>(),
-                x => x.Buffer == inMemoryCrudRepository.FindAllWithAdded<NotificationBuffer>().First());
+            inMemoryCrudRepository.FindAllWithAdded<NotificationBuffer>()
+                .Should().HaveCount(1)
+                .And.ContainSingle(x => x.Name == "buffer1"
+                                                 && x.GovernorName == "governor1"
+                                                 && x.PipelineName == "pipeline1");
+
+            inMemoryCrudRepository.FindAllWithAdded<BufferedNotification>()
+                .Should().HaveCount(1);
+            inMemoryCrudRepository.FindAllWithAdded<BufferedNotification>()
+                .Should().ContainSingle(x => x.Buffer == inMemoryCrudRepository.FindAllWithAdded<NotificationBuffer>().First());
+        }
+
+        [Fact]
+        public async Task SendNotificationAsync_CreatesBufferOnce()
+        {
+            await sut.Add(new SerializedNotification() { NotificationClassName = "Notification1", NotificationJson = "{}" },
+                "buffer1", DateTime.Now, "governor1", "pipeline1");
+            await sut.Add(new SerializedNotification() { NotificationClassName = "Notification1", NotificationJson = "{}" },
+                "buffer1", DateTime.Now, "governor1", "pipeline1");
+
+            inMemoryCrudRepository.FindAllWithAdded<NotificationBuffer>()
+                .Should().HaveCount(1)
+                .And.ContainSingle(x => x.Name == "buffer1"
+                                        && x.GovernorName == "governor1"
+                                        && x.PipelineName == "pipeline1");
+
+            inMemoryCrudRepository.FindAllWithAdded<BufferedNotification>()
+                .Should().HaveCount(2)
+                .And.OnlyContain(x => x.Buffer == inMemoryCrudRepository.FindAllWithAdded<NotificationBuffer>().First());
         }
 
         [Fact]
